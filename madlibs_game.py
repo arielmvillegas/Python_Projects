78ae2af597a1db1f8110685f9c18cb15fbdfd845
arielmvillegas <arielmarievillegas@outlook.com>
2026-02-10 11:49:04 -0800

    Add Mad libs game

---
+++ madlibs_game.py
@@ -0,0 +1,44 @@
+# Madlibs - creating a word game where you create a story by filling in blanks with random words!
+
+# Intro section
+print("🎊 Welcome to the Farmer's Market Mad Libs Adventure! 🧺")
+print("Get ready to make a stroy full of surprises, silly tiwsts, and maybe a little bit of chaos.")
+print("You'll be asked for some words: adjectives, nouns, verbs, and more - so think creatively! 🪿")
+print("")
+
+
+# accept user input()
+adjective1 = input("Enter an adjective (description): ")
+adjective2 = input("Enter an adjective (description): ")
+noun1 = input("Enter a noun (person, place, thing): ")
+adjective3 = input("Enter an adjective (description): ")
+noun2 = input("Enter a noun (person, place, thing): ")
+verb1 = input("Enter a verb ending with 'ing' (describe an action or state): ")
+adjective4 = input("Enter an adjective (description): ")
+noun3 = input("Enter a noun (person, place, thing): ")
+phrase1 = input("Enter a interesting phrase!: ")
+adjective5 = input("Enter an adjective (description): ")
+noun4 = input("Enter a noun (person, place, thing): ")
+phrase2 = input("Enter a funny phrase!: ")
+adjective6 = input("Enter an adjective (description): ")
+noun5 = input("Enter a noun (person, place, thing): ")
+noun6 = input("Enter a noun (person, place, thing): ")
+
+
+print(f"On a {adjective1} morning, I skipped over to the Farmer's market with a {adjective2} attitude. 🧺")
+print(f"The air smelled like {noun1}, fresh bread, and questionable life choices.")
+print(f"Every booth was bursting with {adjective3} fruits, vegetables, and one suspiciously confident {noun2}. 🤔")
+
+print(f"I waved at a vendor who was {verb1} while wearing a {adjective4} apron.")
+print(f"'Care for some {noun3}?' they asked, holding it like it was treasure.")
+print(f"I nodded, mostly because they said, '{phrase1}'.")
+
+print(f"I stopped to sample a {adjective5} snack that tasted like {noun4} and pure joy.💗")
+print(f"Suddenly, someone shouted, '{phrase2}!' and the entire market erupted in cheers.")
+
+print(f"A parade of {adjective6} {noun5} rushed past me, and I somehow fell to the ground.")
+print(f"Before I could say 'hey!', it started raining {noun6}!!! 😲")
+
+print("Suddenly, an annoying noise cut through the chaos.")
+print("I blinked and realized it was my alarm going off. ⏰")
+print("I was late for work but at least I knew Sunday was approaching and so was the farmers market! 🛍️")
